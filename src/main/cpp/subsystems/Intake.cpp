a351b81f9ed3b42c6210a053ab653033cd5a1722
Chris <crho1212@gmail.com>
2022-08-11 20:26:29 -0400

    Added functionality to roller motor + singulator

--- src/main/cpp/subsystems/Intake.cpp
+++ src/main/cpp/subsystems/Intake.cpp
@@ -9,6 +9,16 @@ void Intake::RobotInit()
     intakePivot.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
     intakePivot.BurnFlash();
     intakePivotEncoder.SetPosition(0);
+    intakeRoller.RestoreFactoryDefaults();
+    intakeRoller.SetInverted(false);
+    intakeRoller.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
+    intakeRoller.BurnFlash();
+    intakeRollerEncoder.SetPosition(0);
+    singulator.RestoreFactoryDefaults();
+    singulator.SetInverted(false);
+    singulator.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
+    singulator.BurnFlash();
+    singulatorEncoder.SetPosition(0);
 }
 
 void Intake::RobotPeriodic()
@@ -17,13 +27,33 @@ void Intake::RobotPeriodic()
     if (secondary.GetRawButton(1) && intakePivotEncoder.GetPosition() <= 4.571424)
     {
         intakePivot.Set(.4);
+        intakeRoller.Set(-.4);
     }
     else if (!secondary.GetRawButton(1) && intakePivotEncoder.GetPosition() >= 0)
     {
         intakePivot.Set(-.4);
+        singulatorTimer = 50;
     }
     else 
     {
         intakePivot.Set(0);
+    }
+    if (intakePivotEncoder.GetPosition() > 1)
+    {
+        
+        singulator.Set(.4);
+    }
+    else
+    {
+        intakeRoller.Set(0);
+    }
+    if (singulatorTimer > 0)
+    {
+        singulatorTimer--;
+        singulator.Set(.4);
+    }
+    else if (intakePivotEncoder.GetPosition() < 1)
+    {
+        singulator.Set(0);
     }
 }
--- src/main/include/subsystems/Intake.h
+++ src/main/include/subsystems/Intake.h
@@ -12,4 +12,12 @@ private:
 rev::CANSparkMax intakePivot = rev::CANSparkMax(11, rev::CANSparkMax::MotorType::kBrushless);
 frc::Joystick secondary{ 1 };
 rev::SparkMaxRelativeEncoder intakePivotEncoder = intakePivot.GetEncoder();
+
+rev::CANSparkMax intakeRoller = rev::CANSparkMax(12, rev::CANSparkMax::MotorType::kBrushless);
+rev::SparkMaxRelativeEncoder intakeRollerEncoder = intakeRoller.GetEncoder();
+
+rev::CANSparkMax singulator = rev::CANSparkMax(13, rev::CANSparkMax::MotorType::kBrushless);
+rev::SparkMaxRelativeEncoder singulatorEncoder = singulator.GetEncoder();
+
+int singulatorTimer = 0;
 };
