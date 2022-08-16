680f345be981789921bd23456a0fe9cbd5b3fd32
Benjamin Chatman <bennyjjumpin@gmail.com>
2022-08-16 19:19:03 -0400

    Advanced indexer finctionality
    indexer works in conjunction with intake.

--- src/main/cpp/subsystems/Indexer.cpp
+++ src/main/cpp/subsystems/Indexer.cpp
@@ -5,22 +5,35 @@ void Indexer::RobotPeriodic()
 {
     frc::SmartDashboard::PutBoolean("Top", topBeamBreak.Get());
     frc::SmartDashboard::PutBoolean("Mid", midBeamBreak.Get());
-
-    if(midBeamBreak.Get() == false && topBeamBreak.Get() == false)
+    if(secondary.GetRawButton(1))
+    {
+        if(midBeamBreak.Get() == false && topBeamBreak.Get() == false)
+        {
+            indexerZone1.Set(0);
+        }
+        else
+        {
+            indexerZone1.Set(0.369);
+        }
+        
+        if(topBeamBreak.Get() == false)
+        {
+            indexerZone2.Set(0);
+        }
+        else
+        {
+            indexerZone2.Set(-0.3);
+        }
+    }
+    else if(secondary.GetRawButton(2))
+    {
+        indexerZone1.Set(-0.369);
+        indexerZone2.Set(0.3);
+    }
+    else
     {
         indexerZone1.Set(0);
-    }
-    else
-    {
-        indexerZone1.Set(0.2);
-    }
-    if(topBeamBreak.Get() == false)
-    {
         indexerZone2.Set(0);
-    }
-    else
-    {
-        indexerZone2.Set(-0.2);
     }
 }
 
--- src/main/include/subsystems/Indexer.h
+++ src/main/include/subsystems/Indexer.h
@@ -2,6 +2,7 @@
 
 #include <rev/CANSparkMax.h>
 #include <frc/DigitalInput.h>
+#include <frc/Joystick.h>
 
 class Indexer
 {
@@ -12,5 +13,6 @@ class Indexer
     rev::CANSparkMax indexerZone2 = rev::CANSparkMax(22, rev::CANSparkMax::MotorType::kBrushless);
     frc::DigitalInput midBeamBreak{9};
     frc::DigitalInput topBeamBreak{5};
+    frc::Joystick secondary{1};
 
 };
